605ff7c0529e65629cdc175e36435c3be61ecc6f
Kayla Tryon <dawnka22@gmail.com>
2019-07-03 10:53:32 -0600

    javascript fixes

--- controllers/webScrapperController.js
+++
@@ -1,9 +0,0 @@
-// Dependencies
-const axios = require('axios');
-const cheerio = require('cheerio'); //web scrapper
-const mongoose = require('mongoose');
-const db = require("../models");
-
-// Mongoose
-mongoose.Promise = Promise; 
-mongoose.connect("mongodb")
